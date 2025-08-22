85c9661e86f1c58498a694d983b0156d5aa215af
Michael <michaelseedev@gmail.com>
2025-08-21 20:53:38 -0800

    pulling bulletpoints from db

--- app/_components/BulletList.tsx
+++ app/_components/BulletList.tsx
@@ -1,7 +1,18 @@
-function BulletList() {
-  //temporary bullets - will pull from db
-  const bullets = [];
-  return bullets.map((e) => <BulletPoint />);
-}
+"use client";
 
-export default BulletList;
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "@/app/_lib/dexie/db";
+
+export default function BulletList() {
+  const bullets = useLiveQuery(() => db.bullets.toArray(), []);
+
+  if (!bullets) return null;
+
+  return (
+    <ul>
+      {bullets.map((b) => (
+        <li key={b.id}>{b.text}</li>
+      ))}
+    </ul>
+  );
+}
--- app/builder/[resumeId]/page.tsx
+++ app/builder/[resumeId]/page.tsx
@@ -1,3 +1,4 @@
+import BulletList from "@/app/_components/BulletList";
 import SeedBullets from "@/app/_components/SeedBullets";
 import SideResume from "@/app/_components/SideResume";
 
@@ -7,6 +8,7 @@ export default function Page() {
       <div className="w-64 bg-blue-500">
         <div>Col A</div>
         <SeedBullets />
+        <BulletList />
       </div>
       <div className="flex-1 overflow-auto w-full bg-gray-500 pt-5 px-5 grid items-start justify-items-center">
         <SideResume />
