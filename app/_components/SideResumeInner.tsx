d807d745796abfee027c4253039cc922373734aa
Michael <michaelseedev@gmail.com>
2025-08-23 18:20:28 -0800

    testing tracking height for auto page-break

--- app/_components/SideResumeInner.tsx
+++ app/_components/SideResumeInner.tsx
@@ -3,6 +3,7 @@ import { db } from "../_lib/dexie/db";
 import BulletPoint from "./BulletPoint";
 import ResumeHeader from "./ResumeHeader";
 import JobSection from "./JobSection";
+import { useLayoutEffect, useRef } from "react";
 
 export default function SideResumeInner() {
   const bullets = useLiveQuery(() => db.bullets.toArray(), []);
@@ -10,12 +11,24 @@ export default function SideResumeInner() {
   const PAGE_HEIGHT = 1100;
   const LINE_THICKNESS = 2;
 
+  let spacers = 0;
+
   //TODO 8/23/2025: I am probably going to change this to be page-by-page editing.
   //The problem with continuous editing is that the padding here would only apply to the first page
   //all subsequent pages don't know they're new pages
 
+  const ref = useRef<HTMLDivElement | null>(null);
+  useLayoutEffect(() => {
+    console.log("Here");
+    if (!ref.current) return;
+    // console.log("offsetHeight:", ref.current.offsetHeight); // integer, includes borders
+    console.log("clientHeight:", ref.current.clientHeight); // excludes borders/scrollbar
+    // console.log("rect height:", ref.current.getBoundingClientRect().height); // precise, decimals
+  }, []);
+
   return (
     <div
+      ref={ref}
       style={{
         position: "relative",
         backgroundColor: "white",
@@ -24,33 +37,11 @@ export default function SideResumeInner() {
         fontSize: "16px",
         lineHeight: 1.4,
         padding: "48px", //TODO: will set to 48 when I'm done testing page breaks
-        overflowY: "auto",
+        // overflowY: "auto",
       }}
     >
       <ResumeHeader />
       <JobSection />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
-      <BulletPoint />
       <BulletPoint />
       <BulletPoint />
       <BulletPoint />
@@ -97,13 +88,32 @@ export default function SideResumeInner() {
 }
 
 {
-  /* <BulletPoint id="32b166b8-4243-473d-b98c-1f5ceba95160" text="1" /> */
-}
-{
   /* {Array.from({ length: 52 }, (_, i) => (
         <div key={i}>
           ABCDEFGHIJKLNOPQRSTUVWXYZ ABCDEFGHIJKLNOPQRSTUVWXYZ
           ABCDEFGHIJKLNOPQRSTUVWXYZ ABDEF
         </div>
       ))} */
+}
+
+{
+  //dotted line spacer:
+  /*
+     <div
+        aria-hidden
+        style={{
+          position: "absolute",
+          inset: 0,
+          pointerEvents: "none",
+          zIndex: 10,
+          backgroundImage: `repeating-linear-gradient(
+            to bottom,
+            transparent,
+            transparent ${PAGE_HEIGHT - LINE_THICKNESS}px,
+            rgba(0,0,0,0.35) ${PAGE_HEIGHT - LINE_THICKNESS}px,
+            rgba(0,0,0,0.35) ${PAGE_HEIGHT}px
+          )`,
+        }}
+      />
+  */
 }
