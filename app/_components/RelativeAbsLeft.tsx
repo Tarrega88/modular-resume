f64d93d8c51aee3ff00ad994d8a293e8dda35156
Michael <michaelseedev@gmail.com>
2025-09-07 10:23:10 -0800

    made rel abs container consistent to width of page

--- app/_components/RelativeAbsLeft.tsx
+++ app/_components/RelativeAbsLeft.tsx
@@ -1,8 +1,12 @@
+import { pageWidth } from "./SideResumeInner";
+
 function RelativeAbsLeft({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative">
-      <div className="absolute -left-6 group-hover:opacity-100 opacity-0 transition-all duration-150">
-        {children}
+    <div style={{ width: pageWidth }} className="-translate-x-[48px]">
+      <div className="relative">
+        <div className="absolute left-6 group-hover:opacity-100 opacity-0 transition-all duration-150">
+          {children}
+        </div>
       </div>
     </div>
   );
--- app/_components/RelativeAbsRight.tsx
+++ app/_components/RelativeAbsRight.tsx
@@ -1,8 +1,12 @@
+import { pageWidth, widthWithoutMargin } from "./SideResumeInner";
+
 function RelativeAbsRight({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative">
-      <div className="absolute -right-6 group-hover:opacity-100 opacity-0 transition-all duration-150">
-        {children}
+    <div style={{ width: pageWidth }} className="-translate-x-[48px]">
+      <div className="relative">
+        <div className="absolute right-6 group-hover:opacity-100 opacity-0 transition-all duration-150">
+          {children}
+        </div>
       </div>
     </div>
   );
